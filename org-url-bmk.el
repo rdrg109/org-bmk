79f9bb776a8793df2d990c6411143005bc7ba6f7
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2021-05-11 22:35:38 -0500

    Users can now decide what to save: a marker to the haeading or the actual URL
    Previously, only markers were being stored which forced the user to
    keep their "org-url-bmk-files" opened, now they can kill them because
    the actual URL is stored in the candidates list.

--- org-url-bmk.el
+++ org-url-bmk.el
@@ -1,32 +1,35 @@
-;; TODO
-;;
-;; lint:
-;;   + every search engine must have at least one space character.
-
 (require 'cl-lib)
+(require 'ivy)
 
 (defvar org-url-bmk-frontend 'tridactyl
-  "Variable which defines the formatting for the content of the
-  files `org-url-bmk-file-bookmarks' and
-  `org-url-bmk-file-search-engines'.")
+  "Value which defines the formatting for the correct
+  definitions of bookmarks and search engines in the files
+  `org-url-bmk-file-bookmarks' and
+  `org-url-bmk-file-search-engines', respectively.")
 
-(defvar org-url-bmk-files '("~/my/org/Bookmark manager/Bookmarks.org")
-  "Org files which stores the bookmarks.")
+(defvar org-url-bmk-files nil
+  "Org files which stores the bookmarks.
 
-(defvar org-url-bmk-file-bookmarks "~/.config/tridactyl/tridactyl-bookmarks.vim"
+These files are then used to perform the following actions:
+
++ Export and index bookmarks.
++ Export search engines.")
+
+(defvar org-url-bmk-file-bookmarks nil
   "File in which bookmarks are to be saved.
 
 Don't modify this file manually since its content is deleted
 after executing `org-url-bmk-export-bookmarks'.")
 
-(defvar org-url-bmk-file-search-engines "~/.config/tridactyl/tridactyl-search-engines.vim"
+(defvar org-url-bmk-file-search-engines nil
   "File in which search engines are to be saved.
 
 Don't modify this file manually since its content is deleted
-3after executing `org-url-bmk-export-search-engines'.")
+after executing `org-url-bmk-export-search-engines'.")
 
 (defvar org-url-bmk-property-alias "alias"
-  "Headline property which contain the alias for the current heading")
+  "Headline property which contain the alias for the current
+  heading.")
 
 (defvar org-url-bmk-property-alias-prefix "alias-prefix"
   "Headline property which contain the alias that children
@@ -35,14 +38,53 @@ Don't modify this file manually since its content is deleted
 (defvar org-url-bmk-property-bookmarks "url"
   "Headline property that stores the URL.
 
-This property is required for a heading to be considered a bookmark.")
+Having this property is the only requiremenet for a heading to be
+considered a bookmark.")
 
 (defvar org-url-bmk-property-search-engines "search"
   "Headline property that stores the URL.
 
-The space character is used as the location for the placeholders.
+An space character or matching curly braces can be used as
+locations for placeholders.
 
-This property is required for a heading to be considered a bookmark.")
+Having this property is the only requirement for a heading to be
+considered a search engine.")
+
+(defvar org-url-bmk-prefix-bookmarks nil
+  "String that prefix the shortcut of each bookmark.")
+
+(defvar org-url-bmk-prefix-search-engines nil
+  "String that prefix the shortcut of each search engine.")
+
+(defvar org-url-bmk-cache-data-properties t
+  "Defines whether URLs need to be saved along with each bookmark
+or search engine in `org-url-bmk-indexed-bookmarks' and
+`org-url-bmk-indexed-search-engines'.
+
+If set to nil, then a marker is stored for each bookmark or
+search engine. Doing this implies that the file that stores your
+bookmarks need to remain open in order to be able to retrieve the
+corresponding URL when selecting a bookmark or search engine.")
+
+(defvar org-url-bmk-indexed-bookmarks nil
+  "Stores indexed bookmarks.")
+
+(defvar org-url-bmk-indexed-search-engines nil
+  "Stores indexed search engines.")
+  
+(defun org-url-bmk-sanitize-search-engine (search-engine)
+  "Removes the double quotes that are located at the beginning
+and end of the search engine.
+
+Recall that some search engines contain these characters to
+ensure that the placeholders that might be at the end of the
+URL are not lost when executing `org-entry-get'."
+  (if (and (string-match "^\"" search-engine)
+	   (string-match "\"\\'" search-engine))
+      (replace-regexp-in-string "\"\\'" ""
+				(replace-regexp-in-string "^\"" ""
+							  search-engine))
+    search-engine))
 
 (defun org-url-bmk-export-bookmarks ()
   "Export all bookmarks to `org-url-bmk-file-bookmarks'."
@@ -66,7 +108,7 @@ This property is required for a heading to be considered a bookmark.")
 	(dolist (bookmark bookmarks)
 	  (let ((shortcut (car bookmark))
 		(url (cdr bookmark)))
-	    (insert (format "set searchurls.%s %s\n" shortcut url))))
+	    (insert (format "set searchurls.%s %s\n" (concat org-url-bmk-prefix-bookmarks shortcut) url))))
 	(save-buffer)))))
 
 (defun org-url-bmk-export-search-engines ()
@@ -77,32 +119,39 @@ This property is required for a heading to be considered a bookmark.")
       (with-current-buffer (find-file-noselect file)
 	(org-map-entries (lambda ()
 			   (interactive)
-			   (let (shortcut search-engine alias-prefix alias)
+			   (let (search-engine alias-prefix alias)
 			     (setq search-engine (org-entry-get nil org-url-bmk-property-search-engines))
 			     (when search-engine
 			       (setq alias-prefix (org-entry-get nil org-url-bmk-property-alias-prefix t))
 			       (setq alias (org-entry-get nil org-url-bmk-property-alias))
+			       ;; If one of the following properties have been defined
 			       (when (or alias-prefix alias)
-				 (setq shortcut (org-url-bmk-build-shortcut alias-prefix alias))
-				 (unless (assoc shortcut search-engines)
-				   (when (and (string-match "^\"" search-engine)
-					      (string-match "\"\\'" search-engine))
-				     (setq search-engine (replace-regexp-in-string "\"\\'" "" (replace-regexp-in-string "^\"" "" search-engine))))
-				   (let ((nspaces (cl-count ?  search-engine)) count)
-				     (if (eq nspaces 1)
-					 (setq search-engine (replace-regexp-in-string " " "%s" search-engine))
-				       (if (> nspaces 1)
-					   (progn
-					     (setq count 0)
-					     (setq search-engine (replace-regexp-in-string " " (lambda (_) (format "%%s%d" (cl-incf count))) search-engine)))))
-				   (add-to-list 'search-engines `(,shortcut . ,search-engine)))))))))
-      (with-current-buffer (find-file-noselect org-url-bmk-file-search-engines)
-	(erase-buffer)
-	(dolist (search-engine search-engines)
-	  (let ((shortcut (car search-engine))
-		(url (cdr search-engine)))
-	    (insert (format "set searchurls.%s %s\n" shortcut url))))
-	(save-buffer))))))
+				 (let ((shortcut (org-url-bmk-build-shortcut alias-prefix alias)))
+				   ;; If the shortcut hasn't been used yet.
+				   (unless (assoc shortcut search-engines)
+				     (let ((hierarchy (mapconcat 'identity (org-get-outline-path t) " / ")))
+				       (when (and (string-match "^\"" search-engine)
+						  (string-match "\"\\'" search-engine))
+					 (setq search-engine (replace-regexp-in-string "\"\\'" "" (replace-regexp-in-string "^\"" "" search-engine))))
+				       (let ((nspaces (cl-count ?  search-engine)) count)
+					 (if (eq nspaces 1)
+					     (setq search-engine (replace-regexp-in-string " " "%s" search-engine))
+					   (if (> nspaces 1)
+					       (progn
+						 (setq count 0)
+						 (setq search-engine (replace-regexp-in-string " " (lambda (_) (format "%%s%d" (cl-incf count))) search-engine)))))
+					 (add-to-list 'search-engines `(,hierarchy ,shortcut ,search-engine)))))))))))))
+    (with-current-buffer (find-file-noselect org-url-bmk-file-search-engines)
+      (erase-buffer)
+      (dolist (search-engine search-engines)
+	(let ((hierarchy (car search-engine))
+	      (shortcut (nth 1 search-engine))
+	      (url (nth 2 search-engine)))
+	  (insert (format "\n\" %s\nset searchurls.%s %s\n"
+			  hierarchy
+			  (concat org-url-bmk-prefix-search-engines shortcut)
+			  url))))
+      (save-buffer))))
 
 (defun org-url-bmk-build-shortcut (alias-prefix alias)
   "Build the shortcut given a alias and the alias prefix.
@@ -141,8 +190,141 @@ Returns nil when the two arguments are nil."
 	  (setq alias-prefix (org-entry-get nil org-url-bmk-property-alias-prefix t))
 	  (setq shortcut (org-url-bmk-build-shortcut alias-prefix alias))
 	  (message "Shortcut: %s" shortcut))
-      (message "The subtree at point doesn't have the required
-      properties to be considered a search engine or a
-      bookmark."))))
+      (user-error "The subtree at point doesn't have the required properties to be considered a search engine or a bookmark."))))
+
+(defun org-url-bmk-index-bookmarks ()
+  "Index all bookmarks in `org-url-bmk-files'.
+
+All bookmarks metadata is stored in
+`org-url-bmk-indexed-bookmarks'.
+
+For a definition on bookmarks, read this."
+  (interactive)
+  (setq org-url-bmk-indexed-bookmarks nil)
+  (dolist (file org-url-bmk-files)
+    (with-current-buffer (find-file-noselect file)
+      (org-map-entries
+       (lambda ()
+	 (let ((property (org-entry-get nil org-url-bmk-property-bookmarks)))
+	   (when property
+	     (let ((choice (mapconcat 'identity (org-get-outline-path t) " / "))
+		   (metadata (if org-url-bmk-cache-data-properties
+				 property
+			       (point-marker))))
+	       (add-to-list 'org-url-bmk-indexed-bookmarks `(,choice . ,metadata))))))
+       nil
+       'file)
+      (if org-url-bmk-cache-data-properties
+	  (kill-buffer))))
+  (message "bookmarks have been indexed."))
+
+(defun org-url-bmk-index-search-engines ()
+  "index all search engines in `org-url-bmk-files'.
+
+all search engines metadata is stored in
+`org-url-bmk-indexed-search-engines'.
+
+for a definition on search engines, read this."
+  (interactive)
+  (setq org-url-bmk-indexed-search-engines nil)
+  (dolist (file org-url-bmk-files)
+    (with-current-buffer (find-file-noselect file)
+      (org-map-entries
+       (lambda ()
+	 (let ((property (org-entry-get nil org-url-bmk-property-search-engines)))
+	   (when property
+	     (let ((choice (mapconcat 'identity (org-get-outline-path t) " / "))
+		   (metadata (if org-url-bmk-cache-data-properties
+				 property
+			       (point-marker))))
+	       (add-to-list 'org-url-bmk-indexed-search-engines `(,choice . ,metadata))))))
+       nil
+       'file)
+      (if org-url-bmk-cache-data-properties
+	  (kill-buffer))))
+  (message "Search engines have been indexed."))
+
+(defun org-url-bmk-prompt-bookmark (action)
+  "Prompts the user for a bookmark and perform ACTION."
+  (unless (functionp action)
+    (user-error "The provided ACTION \"%s\" is not a function." action))
+  (ivy-read "Bookmark: " org-url-bmk-indexed-bookmarks
+	    :action (lambda (a)
+		      (let ((metadata (cdr a))
+			    url)
+			(if org-url-bmk-cache-data-properties
+			    (setq url metadata)
+			  (let ((buffer (marker-buffer metadata))
+				(setq url (with-current-buffer buffer
+					    (goto-char marker)
+					    (org-entry-get nil org-url-bmk-property-bookmarks))))))
+			(funcall action url)))))
+
+(defun org-url-bmk-prompt-search-engine (action)
+  "Prompts the user for a search engine, fill placeholders and
+perform ACTION."
+  (unless (functionp action)
+    (user-error "The provided ACTION \"%s\" is not a function." action))
+  (ivy-read "Search engine: " org-url-bmk-indexed-search-engines
+	    :action (lambda (a)
+		      (let* ((metadata (cdr a))
+			     url)
+			(if org-url-bmk-cache-data-properties
+			    (setq url metadata)
+			  (setq url
+				(let* ((marker metadata)
+				       (buffer (marker-buffer marker)))
+				  (with-current-buffer buffer
+				    (goto-char marker)
+				    (org-entry-get nil org-url-bmk-property-search-engines)))))
+			(setq url
+			      (org-url-bmk-prompt-placeholders
+			       (org-url-bmk-sanitize-search-engine url)))
+			(funcall action url)))))
+
+(defun org-url-bmk-prompt-placeholders (url)
+  "Given a search engine, each placeholder is replaced by the
+value returned of prompting the user."
+  (let (occurrence
+	start
+	end
+	query
+	placeholder-name)
+
+    (unless (stringp url)
+      (error "The provided argument is not a string."))
+    
+    (with-temp-buffer
+      (insert url)
+      (goto-char (point-min))
+      (while (setq ocurrence (search-forward-regexp "[ {]" nil t))
+	(goto-char ocurrence)
+	(backward-char)
+	
+	(setq start (point))
+	
+	(if (equal (format "%c" (char-after)) "{")
+	    (progn
+	      (setq end (save-excursion
+			  (forward-sexp)
+			  (point)))
+	      (setq placeholder-name
+		    (string-trim
+		     (buffer-substring-no-properties (+ start 1) (- end 1)))))
+	  (progn
+	    (setq end (+ start 1))
+	    (setq placeholder-name nil)))
+
+	(replace-regexp "." "" nil start end)
+
+	(if placeholder-name
+	    (setq placeholder-value (read-string (concat
+						  "Placeholder ("
+						  placeholder-name
+						  "): ")))
+	  (setq placeholder-value (read-string "Placeholder (empty name): ")))
+	
+	(insert placeholder-value))
+      (buffer-substring-no-properties (point-min) (point-max)))))
 
 (provide 'org-url-bmk)
